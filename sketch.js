a2f602526ac87fb17b46d7f5a339f10bc020954b
jarrellmarc <jarrellmarc@gmail.com>
2024-03-15 21:49:08 -0400

    Add JS logic and CSS styling
    Added nested loops to draw boxes
    Added reset button to remove old grid and prompt for new size
    
    Added styling for button and div/grid

--- sketch.js
+++ sketch.js
@@ -1,10 +1,27 @@
-let gridSize = 16;
-const container = document.querySelector(".container");
-
-for (i = 0; i < gridSize; i++) {
-    for (j = 0; j < gridSize; j++) {
-        const newSquare = document.createElement("div");
-        newSquare.className = "grid-box";
-        container.appendChild(newSquare);
+function makeGrids(size) {
+    //let screen = document.querySelector(".sketch-screen");
+    let screen = document.createElement("div");
+    screen.className = "sketch-screen";
+    container.appendChild(screen);
+    for (let i = 0; i < size; i++) {
+      let column = document.createElement("div");
+      column.classList.add("column");
+      for (let j = 1; j <= size; j++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.style.border = "2px solid black";
+        column.appendChild(row);
+      }
+      screen.appendChild(column);
     }
-}
+  }
+
+const container = document.querySelector(".container");
+makeGrids(4);
+
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener('click', () => {
+    const removed = document.querySelector(".sketch-screen");
+    container.removeChild(removed);
+    makeGrids(prompt("Grid Size"));
+});
